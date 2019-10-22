5bf1402eb4f015c5e99cd606a0237b8167c45157
taramonheim <taramonheim@me.com>
2019-10-22 16:00:14 +0200

    Update text_01.js

--- text_01.js
+++ text_01.js
@@ -1,2 +1,12 @@
-//hallo
-// hallo blub
+//Definiere die aktuelle EUR Währung 
+let EUR = 1
+//Definiere die aktuelle USD Währung 
+let USD = 1.11
+//Ordne der Währung EUR die Währung USD zu 
+// Eurosumme von Benutzer abfragen 
+
+USD = EUR * 1.11 
+EUR = USD / 1.11
+
+// EUR mit Umrechnungskurs multiplizieren 
+// USD Summe ausgeben 
